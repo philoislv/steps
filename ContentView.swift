b9ac5ffa496349b488df6466cf250430ecae2d7b
Andrés Felipe <andres.alzate@sngular.com>
2019-10-06 19:24:36 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Steps
+//
+//  Created by Andres Felipe Alzate Restrepo on 04/10/2019.
+//  Copyright © 2019 Andres Felipe Alzate Restrepo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
